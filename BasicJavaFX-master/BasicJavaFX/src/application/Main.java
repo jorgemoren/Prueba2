b61d5d795792217b9b4739503854e494f47e75aa
Jorge <jorgemorecas@gmail.com>
2019-12-12 21:02:27 +0100

    Prueba Subida

---
+++ BasicJavaFX-master/BasicJavaFX/src/application/Main.java
@@ -0,0 +1,41 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	private AnchorPane mypane;
+
+	// modifica el método start de la clase principal
+	@Override
+	public void start(Stage stage) throws IOException {
+
+		// primero cargamos la vista fxml en el FXMLLoader
+		FXMLLoader loader = new FXMLLoader(getClass().getResource(
+				"../view/forest.fxml"));
+
+		// a�adimos un controlador para inicializar algún componente
+		loader.setController(new SetBlur());
+
+		// ahora sobre el panel mypane cargamos la vista que dejamos en el
+		// FXMLLoader
+		mypane = (AnchorPane) loader.load();
+		
+		// añade la escena al stage y la titula
+		stage.setTitle("Where's the squirrel?");
+		stage.setScene(new Scene(mypane));
+		stage.show();
+	}
+	
+	// arranca la aplicación
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
